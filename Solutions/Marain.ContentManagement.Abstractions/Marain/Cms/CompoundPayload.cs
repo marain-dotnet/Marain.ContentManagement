2a884072c7bf44b59d19320e37bfb0e14e07db0d
Matthew Adams <matthew.adams@endjin.com>
2019-11-11 12:00:01 +0000

    Added a compound payload.

---
+++ Solutions/Marain.ContentManagement.Abstractions/Marain/Cms/CompoundPayload.cs
@@ -0,0 +1,59 @@
+﻿// <copyright file="CompoundPayload.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Marain.Cms
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// A <see cref="IContentPayload"/> representing a composition of other content.
+    /// </summary>
+    public class CompoundPayload : IContentPayload
+    {
+        /// <summary>
+        /// The registered content type for the content payload.
+        /// </summary>
+        public const string RegisteredContentType = "application/vnd.marain.cms.contentpayload.compound";
+        private IList<IContentPayload> children;
+
+        /// <inheritdoc/>
+        public string ContentType
+        {
+            get
+            {
+                return RegisteredContentType;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the child content payloads.
+        /// </summary>
+        public IList<IContentPayload> Children
+        {
+            get { return this.children ?? (this.children = new List<IContentPayload>()); }
+            set { this.children = value; }
+        }
+
+        /// <inheritdoc/>
+        public IContentPayload Copy(bool replaceId)
+        {
+            return new CompoundPayload() { Children = this.Children.Select(c => c.Copy(replaceId)).ToList() };
+        }
+
+        /// <inheritdoc/>
+        public string GetFullTextSearchContent()
+        {
+            var sb = new StringBuilder();
+
+            foreach (IContentPayload child in this.Children)
+            {
+                sb.AppendLine(child.GetFullTextSearchContent());
+            }
+
+            return sb.ToString();
+        }
+    }
+}
