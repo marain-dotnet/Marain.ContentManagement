4f60822bb17bf51039611a14388433a25163d89c
Matthew Adams <matthew.adams@endjin.com>
2019-11-11 11:52:52 +0000

    Added the start of an OpenAPI document for the public API.

---
+++ Solutions/Marain.ContentManagement.OpenApi.Service/Marain/ContentManagement/OpenApi/Internal/ContentMapper.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="ContentMapper.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Marain.ContentManagement.OpenApi.Internal
+{
+    using System;
+    using Marain.Cms;
+    using Menes;
+    using Menes.Hal;
+
+    /// <summary>
+    /// Maps <see cref="Content"/> to and from a <see cref="HalDocument"/>.
+    /// </summary>
+    internal class ContentMapper : IHalDocumentMapper<Content>
+    {
+        /// <inheritdoc/>
+        public void ConfigureLinkMap(IOpenApiLinkOperationMap links)
+        {
+        }
+
+        /// <inheritdoc/>
+        public HalDocument Map(Content resource)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
